7f31b6e7968e3b27638c3e6cb12214cdc4473ad0
Yoelvis Rafael Perez Brito <yoelvis.perez.20017@gmail.com>
2021-03-04 18:53:17 -0400

    Subiendo primer registro con detalle

---
+++ CrearRegistroConDetalle/UI/MenuInicio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrearRegistroConDetalle.UI.Registros;
+
+namespace CrearRegistroConDetalle
+{
+    public partial class MenuInicio : Form
+    {
+        public MenuInicio()
+        {
+            InitializeComponent();
+            this.UsuarioToolStripMenuItem.Click += new EventHandler(this.UsuarioToolStripMenuItemClicked);
+            this.RolesToolStripMenuItem.Click += new EventHandler(this.RolesToolStripMenuItemClicked);
+
+        }
+
+        private void RolesToolStripMenuItemClicked(object sender, EventArgs e)
+        {
+            /*var rRoles = new RegistroRoles();
+            rRoles.MdiParent = this;
+            rRoles.Show();*/
+            var registrarRoles = new RegistroRoles();
+            registrarRoles.Show();
+        }
+
+        private void UsuarioToolStripMenuItemClicked(object sender, EventArgs e)
+        {
+            /*var rUsuarios = new RegistroRoles();
+            rUsuarios.MdiParent = this;
+            rUsuarios.Show();*/
+            var registrarUsuarios = new RegistroUsuarios();
+            registrarUsuarios.Show();
+        }
+    }
+}
